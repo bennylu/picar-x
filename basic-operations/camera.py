43142611039af55439e9abbb92e766c05610e069
Benny Lu <bennylu.tw@gmail.com>
2021-11-25 23:19:04 +0800

    Update camera.py

--- basic-operations/camera.py
+++ basic-operations/camera.py
@@ -4,5 +4,9 @@ import time
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
-    time.sleep(2)
-    camera.capture('foo.jpg')
+    
+    try:
+        time.sleep(2)
+        camera.capture('foo.jpg')
+    finally:
+        camera.stop_preview()
